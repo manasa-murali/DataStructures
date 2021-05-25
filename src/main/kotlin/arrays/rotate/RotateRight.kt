6260800630644bd958c05ec9bfcd954ad63749ad
manasa-5113 <manasa.m@zohocorp.com>
2021-05-25 20:59:19 +0530

    Array Rotation

---
+++ src/main/kotlin/arrays/rotate/RotateRight.kt
@@ -0,0 +1,32 @@
+package arrays.rotate
+
+fun main(args: Array<String>) {
+    val arrayOf = arrayOf(1, 2,3,4,5,6,7)
+    val rotateBy = 2
+    val result = rotate(arrayOf.toIntArray(), rotateBy)
+    println(result.contentToString())
+
+}
+
+fun rotate(array: IntArray, rotateBy: Int): IntArray {
+    if (array.isEmpty() || array.size == rotateBy ){
+        return array
+    }
+    val k= Math.floorMod(rotateBy,array.size)
+    reverse(array,array.size-k,array.size-1)
+    reverse(array,0,array.size-k-1)
+    reverse(array,0,array.size-1)
+    return array
+}
+
+private fun reverse(array: IntArray, start: Int, end: Int) {
+    var i = start
+    var j= end
+    while (i<j){
+        val temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+        i++
+        j--
+    }
+}
--- src/main/kotlin/arrays/rotate/RotationBasic.kt
+++ src/main/kotlin/arrays/rotate/RotationBasic.kt
@@ -5,6 +5,8 @@ package arrays.rotate
  2> ROTATE BY ONE N TIMES,WITH ONE TEMP SPACE
  3> JUGGLING ALGORITHM, GCD
  4> REVERSI - THIS ONE
+ for left rotattion, take 0 to k-1
+ for right rotation take, array size-k to array size
 */
 fun main(){
     val array = arrayOf(1, 2, 3, 4, 5, 6, 7)
